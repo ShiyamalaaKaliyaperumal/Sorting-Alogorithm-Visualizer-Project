4c18d3b6201a0c0dd4dcc99e8b97b779e5308df1
ShiyamalaaKaliyaperumal <114873369+ShiyamalaaKaliyaperumal@users.noreply.github.com>
2023-05-25 00:25:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sorting-alogorithm-visualizer-project.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
